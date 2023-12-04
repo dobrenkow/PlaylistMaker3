87b3750ad16d9b2e50678cafea51ae7f90ce12ea
fedor <dobrenkow2012@yandex.ru>
2023-12-04 23:07:04 +0300

    1 - commit (11 sprint)

---
+++ app/src/main/java/com/example/playlistmaker3/ResponseTrack.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker3
+
+data class ResponseTrack(
+    val resultCount: Int,
+    val results: List<Track>
+)
--- app/src/main/java/com/example/playlistmaker3/Track.kt
+++ app/src/main/java/com/example/playlistmaker3/Track.kt
@@ -1,8 +1,27 @@
 package com.example.playlistmaker3
 
+import com.google.gson.annotations.SerializedName
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
-    val trackName:String,
-    val artistName:String,
-    val trackTime:String,
-    val artworkUrl100:String
-)
+    @SerializedName("trackName") val trackName:String,
+    @SerializedName("artistName") val artistName:String,
+    @SerializedName("trackTimeMillis") val trackTimeMillis:Long,
+    @SerializedName("artworkUrl100") val artworkUrl100:String
+){
+    fun getFormattedTrackTime(): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+    }
+}
+
+object TrackApiService {
+    private const val BASE_URL = "https://itunes.apple.com/"
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    val create: APICollection = retrofit.create(APICollection::class.java)
+}
